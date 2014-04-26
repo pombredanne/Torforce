57f770fd3bd6efc94b1ac07f45cfb6e76915356f
Daapii <devdaapii@gmail.com>
2014-04-26 14:16:45 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Torforce
+========
+Bruteforces .onion domain names to discover hidden websites inside the TOR network.
+if desired you are able to change the top level domain to something else than
+'.onion'.
+
+
+Usage
+=====
+The basic usage of the script is:
+
+```
+optional arguments:
+  -h, --help            show this help message and exit
+  -n NUMBER_OF_LINKS, --number-of-links NUMBER_OF_LINKS
+  -t TOP_LEVEL_DOMAIN, --top-level-domain TOP_LEVEL_DOMAIN
+```
---
+++ torforce.py
@@ -0,0 +1,35 @@
+import random
+from itertools import product
+import argparse
+
+# Author:   Daxda
+# Date:     26.04.2014
+#
+# Description:
+# Bruteforces a defined amount of randomly generated URLs to discover .onion sites.
+# Alternatively you are able to modify the top level domain to something other
+# than '.onion'.
+
+def main(args):
+    domain = ".onion"
+    if args.top_level_domain:
+        if args.top_level_domain[0] != ".":
+            domain = "." + args.top_level_domain
+        else:
+            domain = args.top_level_domain
+    for _ in range(1, args.number_of_links):
+        alpha = list("1eFGHIJKLM2NOP6QRs5tuvabcdw3xyzABC7DE4fghijklm8no0pqrST9UVWXYZ")
+        random.shuffle(alpha)
+        random.shuffle(alpha)
+        random.shuffle(alpha)
+        print "http://" + "".join(alpha)[:16] + domain
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--number-of-links", type=int, required=True,
+                        help="The amount of links the script should generate.")
+    parser.add_argument("-t", "--top-level-domain", required=False,
+                        help="The top level domain to append to the generated URL,"+\
+                             " by default it generates .onion links.")
+    args = parser.parse_args()
+    main(args)
